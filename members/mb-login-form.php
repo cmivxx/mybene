b755bbbdc05e828fbf9b882c7751ca1cbc660529
Chris Miller <chris@intellagentbenefits.com>
2014-08-18 12:33:26 -0500

    updated style of login form and updated readme

--- members/mb-login-form.php
+++ members/mb-login-form.php
@@ -74,7 +74,11 @@ if (!$session->is_logged_in()) {
             <input name="password" type="password" id="" tabindex="20" value="" size="25" />
             
     <br />
-              <input type="submit" name="login-submit" id="" value="Sign in" />
+              <input type="submit" name="login-submit" id="" value="Sign in" style="
+    background: url(http://demo4.intellagentbenefits.com/wp-content/uploads/2014/08/Sign-In-Button.png) no-repeat;
+    width:  67px;
+    height: 28px;
+    border:  none;" />
               
              <!-- <a href="#">Lost password?</a> -->
               
