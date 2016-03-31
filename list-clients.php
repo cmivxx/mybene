8ba2142e2a210b0a88b2d23d2464de68cf09419e
Chris Miller <chris@intellagentbenefits.com>
2016-03-30 23:19:19 -0500

    refactored the client list page.

--- list-clients.php
+++ list-clients.php
@@ -1,33 +1,26 @@
 <?php 
 
-/*
-
-	List Clients 
-
-*/
+/*  List Clients  */
 
 function list_clients() {
-		
-	global $wpdb;
+    global $wpdb;
 	
-	if (  LOGIN_PAGE == NULL ||  MEMBER_PAGE == NULL ) { 
+    if (  LOGIN_PAGE == NULL ||  MEMBER_PAGE == NULL ) { 
 		echo ERR_MSG_01;
 	}		
 
-	
-		if(isset($_GET['page'])) {
-				$cur_admin_page = $_GET['page'];
-		}
+    if(isset($_GET['page'])) {
+        $cur_admin_page = $_GET['page'];
+    }
 		
-		if($_POST['task'] == 'delgroup'){
-			if(isset($_POST['delete']))
-			
-			$del_ids = $_POST['delete'];
-						
+    if($_POST['task'] == 'delgroup') {
+        if(isset($_POST['delete'])) {
+  			$del_ids = $_POST['delete'];
 			if ( $del_ids == NULL ) {
-				?><div class="updated">
-                
-  <p><strong>You must first select an insured(s) to delete</strong></p></div> <?php
+?>
+	<div class="updated">
+		<p><strong>You must first select an insured(s) to delete</strong></p></div> 
+<?php
 			} else {
 				foreach($del_ids as $del_id){
 					
@@ -39,66 +32,62 @@ function list_clients() {
 					$wpdb->query("DELETE FROM ".FILES_TABLE." WHERE client_id = $del_id");
 				}
 					
-				?><div class="updated">
-    <p><strong>Insured(s) was deleted</strong></p></div> <?php
+?>
+	<div class="updated">
+		<p><strong>Insured(s) was deleted</strong></p></div> 
+<?php
 			}
-		}
-			
-	
- 	$clients_list = $wpdb->get_results("SELECT * FROM ".CLIENTS_TABLE." ORDER BY company ASC ");
-	
+		}		
+
+	$clients_list = $wpdb->get_results("SELECT * FROM ".CLIENTS_TABLE." ORDER BY company ASC ");
 				
-	  echo '<div class="wrap">';
-		echo "<h2>Policy Briefcase Insureds List</h2>";
+	echo '<div class="wrap">';
+	echo "<h2>Policy Briefcase Insureds List</h2>";
 
 ?>
 
-        <form name="form1" method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
-		<input type="hidden" value="delgroup" name="task"/>
+	<form name="form1" method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+	<input type="hidden" value="delgroup" name="task"/>
         
-        <p class="submit">
-        <input type="submit" class="button-secondary delete" name="deleteit" value="Delete Client(s)" onclick="return confirm('WARNING: Are you sure you want to delete? This action will remove all related information for this client including all myBenefits files.')"/>
-        </p>
+	<p class="submit">
+	<input type="submit" class="button-secondary delete" name="deleteit" value="Delete Client(s)" onclick="return confirm('WARNING: Are you sure you want to delete? This action will remove all related information for this client including all myBenefits files.')"/>
+	</p>
 
     <table class="widefat">
-                <thead>
-                    <tr class="thead">
-                        <th scope="col"></th>
-                        <th scope="col">Company Name</th>
-                        <th scope="col">Username</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    if(isset($clients_list)){
-                        foreach($clients_list as $clients){
-                    
-                    ?>
-                    
-                    <tr class="alternate" id="group-<?php echo $clients->id; ?>">
-                    
-                        <th class="check-column" scope="row"><input type="checkbox" value="<?php echo $clients->id; ?>" name="delete[]"/></th>
-                        
-                        <td><?php echo $clients->company; ?></td>
-                        <td><?php echo $clients->email; ?></td>
-                        <td>
-                        <a href="?page=client_file_check&id=<?php echo $clients->id; ?>">&check;</a>
-                        <a href="?page=add_client&id=<?php echo $clients->id; ?>">
-                            <img src="<?php echo IMG_DIRECTORY.'edit.png' ?>" align="absmiddle" title="Edit Client Information"></a>  
-                        <a href="?page=benefits_page&id=<?php echo $clients->id; ?>">
-                            <img src="<?php echo IMG_DIRECTORY.'active.png' ?>" align="absmiddle" title="Add/Edit Client Benefits"></a> 
-                        </td>
-                    </tr>
-                        <?php
-                        }
-                    }
-                    ?>
-                </tbody>
-            </table>        
-                </form>
+		<thead>
+		<tr class="thead">
+			<th scope="col"></th>
+			<th scope="col">Company Name</th>
+			<th scope="col">Username</th>
+			<th scope="col"></th>
+		</tr>
+		</thead>
+		<tbody>
+<?php
+	if(isset($clients_list)){
+		foreach($clients_list as $clients){
+?>
+		<tr class="alternate" id="group-<?php echo $clients->id; ?>">
+			<th class="check-column" scope="row"><input type="checkbox" value="<?php echo $clients->id; ?>" name="delete[]"/></th>
+			<td><?php echo $clients->company; ?></td>
+			<td><?php echo $clients->email; ?></td>
+			<td>
+				<a href="?page=client_file_check&id=<?php echo $clients->id; ?>">&check;</a>
+				<a href="?page=add_client&id=<?php echo $clients->id; ?>">
+				<img src="<?php echo IMG_DIRECTORY.'edit.png' ?>" align="absmiddle" title="Edit Client Information"></a>  
+				<a href="?page=benefits_page&id=<?php echo $clients->id; ?>">
+				<img src="<?php echo IMG_DIRECTORY.'active.png' ?>" align="absmiddle" title="Add/Edit Client Benefits"></a> 
+			</td>
+		</tr>
+<?php
+		}
+	}
+?>
+		</tbody>
+	</table>        
+	</form>
 </div>
 
-<?php }
-
+<?php 
+}
 ?>
